b9251f232c6f093e882b455497445d000f6a2b9e
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-24 11:26:06 +0000

    setting up relatonships in models

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
     /** @use HasFactory<\Database\Factories\QuestionFactory> */
     use HasFactory;
+
+    public function quiz() : BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+
+    public function answers() : HasMany
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use Database\Factories\QuizFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Quiz extends Model
 {
-    /** @use HasFactory<\Database\Factories\QuizFactory> */
+    /** @use HasFactory<QuizFactory> */
     use HasFactory;
+
+    public function questions() : HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
 }
