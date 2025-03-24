df1c25eda8c59590bbe3178d0573e64cb9481d63
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-24 15:55:21 +0000

    http tests for answer api controller. composer pint ran

--- app/Http/Requests/CreateAnswerRequest.php
+++ app/Http/Requests/CreateAnswerRequest.php
@@ -6,14 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateAnswerRequest extends FormRequest
 {
-
     public function rules(): array
     {
         return [
-            'answer' => "required|string",
-            'correct' => "required|boolean",
+            'answer' => 'required|string',
+            'correct' => 'required|boolean',
             'question_id' => 'required|integer|exists:questions,id',
-            'feedback' => "nullable|string",
+            'feedback' => 'nullable|string',
         ];
     }
 }
--- tests/Feature/AnswerApiControllerTest.php
+++ tests/Feature/AnswerApiControllerTest.php
@@ -2,11 +2,47 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Question;
+use App\Models\Quiz;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class AnswerApiControllerTest extends TestCase
 {
+    use DatabaseMigrations;
 
+    public function test_answer_api_controller_successful_creation(): void
+    {
+        $answerData = [
+            'answer' => 'answer',
+            'correct' => true,
+            'question_id' => 1,
+            'feedback' => null,
+        ];
+
+        $quiz = Quiz::factory()->create(['id' => 1]);
+        $question = Question::factory()->create(['id' => 1, 'quiz_id' => $quiz->id]);
+
+        $response = $this->postJson('/api/answers', $answerData);
+        $response->assertStatus(201)
+            ->assertJson(function (AssertableJson $response) {
+                $response->has('message');
+            });
+        $this->assertDatabaseHas('answers', $answerData);
+    }
+
+    public function test_answer_api_controller_invalid_data(): void
+    {
+        $answerData = [
+            'answer' => 1,
+            'correct' => 'false',
+            'question_id' => 'wrong',
+            'feedback' => null,
+        ];
+        $quiz = Quiz::factory()->create(['id' => 1]);
+        $question = Question::factory()->create(['id' => 1, 'quiz_id' => $quiz->id]);
+        $response = $this->postJson('/api/answers', $answerData);
+        $response->assertInvalid(['answer', 'correct', 'question_id']);
+    }
 }
