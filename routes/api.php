f51a05253e86e498201cfa16c87f189a1757f9a6
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-03-24 13:47:57 +0000

    Create route for adding new questions

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\QuestionApiController;
 use App\Http\Controllers\QuizApiController;
 use Illuminate\Support\Facades\Route;
 
 route::get('/quizzes', [QuizApiController::class, 'all']);
+route::post('/questions',[QuestionApiController::class, 'create']);
