cacb7744d3d62f4b1acf235d49f79549d4b5e9b4
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-24 13:19:34 +0000

    tests written for getting all quizzes.

--- tests/Feature/QuizApiControllerTest.php
+++ tests/Feature/QuizApiControllerTest.php
@@ -10,14 +10,29 @@ use Tests\TestCase;
 class QuizApiControllerTest extends TestCase
 {
     use DatabaseMigrations;
-    public function test_QuizApiController_returnsCorrectData() : void
+
+    public function test_quiz_api_controller_returns_correct_data(): void
     {
         $quiz = Quiz::factory()->count(5)->create();
         $response = $this->get('/api/quizzes');
         $response->assertStatus(200)
             ->assertJson(function (AssertableJson $response) {
-                $response->hasAll('message', 'data');
+                $response->hasAll('message', 'data')
+                    ->has('data', 5, function (AssertableJson $data) {
+                        $data->hasAll('id', 'name', 'description')
+                            ->where('name', fn ($name) => is_string($name))
+                            ->where('description', fn ($description) => is_string($description));
+                    });
             });
+    }
 
+    public function test_quiz_api_controller_no_quizzes_exist(): void
+    {
+        $response = $this->get('/api/quizzes');
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $response) {
+                $response->hasAll('message', 'data')
+                    ->where('data', []);
+            });
     }
 }
