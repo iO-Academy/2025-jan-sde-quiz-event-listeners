7f8a81d9cd38fba84068cb99bd40ee14708d5d41
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-03-25 14:39:27 +0000

    Create update method to edit an existing answer

--- app/Http/Controllers/AnswerApiController.php
+++ app/Http/Controllers/AnswerApiController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateAnswerRequest;
+use App\Http\Requests\AnswerRequest;
 use App\Models\Answer;
 use Illuminate\Http\JsonResponse;
 
 class AnswerApiController extends Controller
 {
-    public function create(CreateAnswerRequest $request): JsonResponse
+    public function create(AnswerRequest $request): JsonResponse
     {
         $answer = new Answer;
         $answer->answer = $request->answer;
@@ -24,6 +24,23 @@ class AnswerApiController extends Controller
 
         return response()->json([
             'message' => 'Answer creation failed',
+        ], 500);
+    }
+
+    public function update(AnswerRequest $request, Answer $answer): JsonResponse
+    {
+        $answer->answer = $request->answer ?? $answer->answer;
+        $answer->correct = $request->correct ?? $answer->correct;
+        $answer->feedback = $request->feedback ?? $answer->feedback;
+
+        if ($answer->save()) {
+            return response()->json([
+                'message' => 'Answer edited',
+            ],200);
+        };
+
+        return response()->json([
+            'message' => 'Answer editing failed',
         ], 500);
     }
 }
