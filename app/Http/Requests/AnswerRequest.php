71a52811071433340d29c711960dc9edf0ef91fa
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-03-25 14:38:38 +0000

    Rename file and add conditional logic for validation rules based on type of request

--- app/Http/Requests/AnswerRequest.php
+++ app/Http/Requests/AnswerRequest.php
@@ -4,15 +4,20 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateAnswerRequest extends FormRequest
+class AnswerRequest extends FormRequest
 {
     public function rules(): array
     {
-        return [
+        $rules = [
             'answer' => 'required|string|max:255',
             'correct' => 'required|boolean',
-            'question_id' => 'required|integer|exists:questions,id',
             'feedback' => 'nullable|string|max:255',
         ];
+
+        if($this->isMethod('POST')) {
+            $rules['question_id'] = 'required|integer|exists:questions,id';
+        }
+
+        return $rules;
     }
 }
