16daeeb3e8ed13a8fee052251aa827af0ac5f0cf
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-25 11:00:06 +0000

    created results function in quiz api controller.

--- app/Http/Controllers/QuizApiController.php
+++ app/Http/Controllers/QuizApiController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateQuizRequest;
+use App\Models\Answer;
+use App\Models\Question;
 use App\Models\Quiz;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class QuizApiController extends Controller
 {
@@ -48,5 +51,51 @@ class QuizApiController extends Controller
         return response()->json([
             'message' => 'Quiz creation failed',
         ], 500);
+    }
+
+    public function results(Request $request): JsonResponse
+    {
+        $request->validate([
+            'quiz' => 'required|integer|exists:quizzes,id',
+            'answers' => "required|array",
+            'answers.*.question' => 'required|integer|exists:questions,id',
+            'answers.*.answer' => 'required|integer|exists:answers,id',
+        ]);
+
+        $question_count = 0;
+        $correct_count = 0;
+        $available_points = 0;
+        $points = 0;
+
+        $answers = $request->answers;
+
+        foreach ($answers as $answer) {
+            // increment question count
+            $question_count++;
+            // retrieve data of the question
+            $questionData = Question::findOrFail($answer['question']);
+            // add points to available points from each question
+            $available_points += $questionData->points;
+
+            // retrieve answer data
+            $answerData = Answer::findOrFail($answer['answer']);
+            // if the answer is correct
+            if ($answerData['correct']) {
+                // increment correct count
+                $correct_count++;
+                // add points of question to our points
+                $points += $questionData->points;
+            }
+        }
+
+        return response()->json([
+            'message' => 'Score calculated',
+            'data' => [
+                'question_count' => $question_count,
+                'correct_count' => $correct_count,
+                'available_points' => $available_points,
+                'points' => $points,
+            ],
+        ]);
     }
 }
