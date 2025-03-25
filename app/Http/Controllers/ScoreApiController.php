d16aff6ee9fca711350ae593ab2e09e798553d5a
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-25 16:06:25 +0000

    updated method started

--- app/Http/Controllers/ScoreApiController.php
+++ app/Http/Controllers/ScoreApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ResultsRequest;
 use App\Models\Answer;
 use App\Models\Question;
+use App\Models\Quiz;
 use Illuminate\Http\JsonResponse;
 
 class ScoreApiController extends Controller
@@ -17,26 +18,55 @@ class ScoreApiController extends Controller
         $available_points = 0;
         $points = 0;
 
+        $quizId = $request->quiz;
         $answers = $request->answers;
 
-        foreach ($answers as $answer) {
-            // increment question count
-            $question_count++;
-            // retrieve data of the question
-            $questionData = Question::findOrFail($answer['question']);
-            // add points to available points from each question
-            $available_points += $questionData->points;
+        // retrieve all data for that quiz
+        // with all questions and answers
+        $quiz = Quiz::with('questions.answers')->find($quizId);
 
-            // retrieve answer data
-            $answerData = Answer::findOrFail($answer['answer']);
-            // if the answer is correct
-            if ($answerData['correct']) {
-                // increment correct count
+        // Loop through $quiz->questions to calculate available points
+        // and the number of questions
+        foreach($quiz->questions as $question) {
+            $available_points += $question->points;
+            $question_count++;
+        }
+
+        // Loop through the users answers, find the answer for the id they selected
+        // Calculate points scored for each correct answer
+        foreach ($answers as $answer) {
+            $question = $quiz->questions->find($answer['question']);
+
+            $answerData = $question->answers->find($answer['answer']);
+            if ($answerData && $answerData->correct){
                 $correct_count++;
-                // add points of question to our points
-                $points += $questionData->points;
+                $points += $question->points;
             }
         }
+
+//        // for each question answered
+//        foreach ($answers as $answer) {
+//            // increment question count
+//            $question_count++;
+//            // find the question data for that quiz
+//            $questionData = $quiz->questions->find($answer['question']);
+//
+//            if ($questionData) {
+//                // adding point to available points for every question
+//                $available_points += $questionData->points;
+//            }
+//            //for each question, find the answer data
+//            //that matches the answer id given by the front end
+//            foreach ($quiz->questions as $question) {
+//                $answerData = $question->answers->find($answer['answer']);
+//
+//                if ($answerData && $answerData->correct) {
+//                    $correct_count++;
+//                    $points += $question->points;
+//                }
+//            }
+//        }
+
 
         return response()->json([
             'message' => 'Score calculated',
