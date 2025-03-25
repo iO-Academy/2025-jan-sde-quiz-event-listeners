f19a4f76809c1af77eae8c9589cc17a968013974
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-25 09:12:35 +0000

    classes were not imported

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AnswerApiController;
+use App\Http\Controllers\QuestionApiController;
 use App\Http\Controllers\QuizApiController;
 use Illuminate\Support\Facades\Route;
 
