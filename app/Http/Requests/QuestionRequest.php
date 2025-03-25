42d590253cb9e2585eed8115c752a8dd7aa6d70b
Henry Shanahan <henry.shanahan@gmail.com>
2025-03-25 14:23:02 +0000

    added if statement to request for quizID on editin question

--- app/Http/Requests/QuestionRequest.php
+++ app/Http/Requests/QuestionRequest.php
@@ -8,6 +8,14 @@ class QuestionRequest extends FormRequest
 {
     public function rules(): array
     {
+        if ($this->method() === 'PUT') {
+            return [
+                'question' => 'required|string|min:5|max:70',
+                'hint' => 'nullable|string|min:5|max:70',
+                'points' => 'required|integer',
+            ];
+        }
+
         return [
             'question' => 'required|string|min:5|max:70',
             'hint' => 'nullable|string|min:5|max:70',
