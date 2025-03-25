1b548918f71fdbc9aeafa4ad9b19b044557fa312
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-25 09:43:52 +0000

    tests for invalid question id and empty data

--- tests/Feature/AnswerApiControllerTest.php
+++ tests/Feature/AnswerApiControllerTest.php
@@ -45,4 +45,44 @@ class AnswerApiControllerTest extends TestCase
         $response = $this->postJson('/api/answers', $answerData);
         $response->assertInvalid(['answer', 'correct', 'question_id']);
     }
+
+    public function test_answer_api_controller_question_id_doesnt_exist() : void
+    {
+        $answerData = [
+            'answer' => 'answer',
+            'correct' => true,
+            'question_id' => 5000,
+            'feedback' => null,
+        ];
+        $quiz = Quiz::factory()->create(['id' => 1]);
+        $question = Question::factory()->create(['id' => 1, 'quiz_id' => $quiz->id]);
+
+        $response = $this->postJson('/api/answers', $answerData);
+        $response->assertStatus(422)
+            ->assertJson(function (AssertableJson $response) {
+                $response->hasAll('message', 'errors')
+                    ->has('errors', function (AssertableJson $errors) {
+                        $errors->has('question_id')
+                            ->whereType('question_id', 'array');
+                    });
+            });
+    }
+
+    public function test_api_answer_controller_empty_data() : void
+    {
+        $answerData = [];
+        $response = $this->postJson('/api/answers', $answerData);
+        $response->assertStatus(422)
+            ->assertJson(function (AssertableJson $response) {
+                $response->hasAll('message', 'errors')
+                    ->has('errors', function (AssertableJson $errors) {
+                        $errors->hasAll('answer', 'correct', 'question_id')
+                            ->whereAllType([
+                                'answer' => 'array',
+                                'correct' => 'array',
+                                'question_id' => 'array',
+                            ]);
+                    });
+            });
+    }
 }
