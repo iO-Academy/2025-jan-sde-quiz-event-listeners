feb7d122aae288d0fb21fee2220caf0ab1ebcbb8
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-26 09:30:15 +0000

    front end fixes

--- app/Http/Controllers/AnswerApiController.php
+++ app/Http/Controllers/AnswerApiController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateAnswerRequest;
+use App\Http\Requests\AnswerRequest;
 use App\Models\Answer;
 use Illuminate\Http\JsonResponse;
 
 class AnswerApiController extends Controller
 {
-    public function create(CreateAnswerRequest $request): JsonResponse
+    public function create(AnswerRequest $request): JsonResponse
     {
         $answer = new Answer;
         $answer->answer = $request->answer;
